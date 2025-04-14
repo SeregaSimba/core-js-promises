3eded725698aca852f320379f822ca79b2041b39
Sergei <143517331+SeregaSimba@users.noreply.github.com>
2025-04-14 19:26:56 +0300

    init: task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -17,8 +17,14 @@
  * 0    => promise that will be fulfilled
  * 1    => promise that will be fulfilled
  */
-function getPromise(/* number */) {
-  throw new Error('Not implemented');
+function getPromise(number) {
+  const prom = new Promise((fel, ret) => {
+    if (number >= 0) {
+      fel(number);
+    }
+    ret(number);
+  });
+  return prom;
 }
 
 /**
@@ -33,8 +39,11 @@ function getPromise(/* number */) {
  * Promise.resolve('success') => promise that will be fulfilled with 'success' value
  * Promise.reject('fail')     => promise that will be fulfilled with 'fail' value
  */
-function getPromiseResult(/* source */) {
-  throw new Error('Not implemented');
+function getPromiseResult(source) {
+  const prom = new Promise((resolve) => {
+    source.then(() => resolve(`success`)).catch(() => resolve(`fail`));
+  });
+  return prom;
 }
 
 /**
@@ -50,8 +59,9 @@ function getPromiseResult(/* source */) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  const prom = Promise.any(promises);
+  return prom;
 }
 
 /**
@@ -73,8 +83,9 @@ function getFirstResolvedPromiseResult(/* promises */) {
  * [promise3, promise6, promise2] => Promise rejected with 2
  * [promise3, promise4, promise6] => Promise rejected with 6
  */
-function getFirstPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstPromiseResult(promises) {
+  const prom = Promise.race(promises);
+  return prom;
 }
 
 /**
@@ -88,8 +99,9 @@ function getFirstPromiseResult(/* promises */) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)] => Promise rejected with 2
  */
-function getAllOrNothing(/* promises */) {
-  throw new Error('Not implemented');
+function getAllOrNothing(promises) {
+  const prom = Promise.all(promises);
+  return prom;
 }
 
 /**
@@ -104,8 +116,11 @@ function getAllOrNothing(/* promises */) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  const prom = Promise.all(
+    promises.map((promis) => promis.then((res) => res).catch(() => null))
+  );
+  return prom;
 }
 
 /**
@@ -126,8 +141,12 @@ function getAllResult(/* promises */) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuePromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuePromises(promises) {
+  const prom = promises.reduce(
+    (res, pro) => res.then((p) => pro.then((val) => `${p}${val}`)),
+    Promise.resolve('')
+  );
+  return prom;
 }
 
 module.exports = {
